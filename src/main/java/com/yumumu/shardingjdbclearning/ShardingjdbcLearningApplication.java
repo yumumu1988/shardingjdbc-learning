ef86c953e3edf4e2b0e305de5abe55cae50a3669
zhanghailin <zhanghailin@aixuexi.com>
2022-06-01 17:34:41 +0800

    initial

---
+++ src/main/java/com/yumumu/shardingjdbclearning/ShardingjdbcLearningApplication.java
@@ -0,0 +1,13 @@
+package com.yumumu.shardingjdbclearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingjdbcLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingjdbcLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yumumu/shardingjdbclearning/ShardingjdbcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumumu.shardingjdbclearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingjdbcLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
