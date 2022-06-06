3749acaf759e7deda1cf19db18a27747fb47ff18
zhanghailin <zhanghailin@aixuexi.com>
2022-06-06 15:59:36 +0800

    easy scan

--- src/main/java/com/yumumu/shardingjdbclearning/ShardingjdbcLearningApplication.java
+++ src/main/java/com/yumumu/shardingjdbclearning/ShardingjdbcLearningApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@MapperScan("com.yumumu.shardingjdbclearning.user.dao")
-@MapperScans({@MapperScan("com.yumumu.shardingjdbclearning.user.dao"),
-		@MapperScan("com.yumumu.shardingjdbclearning.ship.dao"),
-		@MapperScan("com.yumumu.shardingjdbclearning.userLog.dao"),
-		@MapperScan("com.yumumu.shardingjdbclearning.dictionary.dao")})
+@MapperScan("com.yumumu.shardingjdbclearning.*.dao")
+//@MapperScans({@MapperScan("com.yumumu.shardingjdbclearning.user.dao"),
+//		@MapperScan("com.yumumu.shardingjdbclearning.ship.dao"),
+//		@MapperScan("com.yumumu.shardingjdbclearning.userLog.dao"),
+//		@MapperScan("com.yumumu.shardingjdbclearning.dictionary.dao")})
 public class ShardingjdbcLearningApplication {
 
 	public static void main(String[] args) {
